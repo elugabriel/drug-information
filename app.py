64f0aa374e3d6f8e07a3c5964dfd2085c98f54cb
elugabriel <gabrielelufidodo2016@gmail.com>
2023-04-10 01:47:43 +0100

    added database functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -23,13 +23,47 @@ class Drug(db.Model):
     
 @app.route('/')
 def index():
-    drug = Drug.query.with_entities(Drug.name, 
+    return render_template('index.html')
+
+@app.route("/get_drug", methods=['GET', 'POST'])
+def get_drug():
+    sickness_info = request.form.get('sickness')
+    size_info = request.form.get('size')
+   
+
+    if request.form['sickness'] != '' and  request.form['size'] != '':
+        drug = Drug.query.with_entities(Drug.name, 
                                     Drug.sickness,
                                     Drug.trans_sickness,
                                     Drug.indication,
                                     Drug.trans_indication,
-                                    Drug.size).all()
-    return render_template('index.html')
+                                    Drug.size).filter_by(trans_sickness= sickness_info, size= size_info).all()
+        return render_template('index.html', drug = drug)
+  
+    if request.form['sickness'] != '' and  request.form['size'] == '':
+        drug = Drug.query.with_entities(Drug.name, 
+                                    Drug.sickness,
+                                    Drug.trans_sickness,
+                                    Drug.indication,
+                                    Drug.trans_indication,
+                                    Drug.size).filter_by(trans_sickness= sickness_info).all()
+        return render_template('index.html', drug = drug)
+  
+    if request.form['sickness'] == '' and  request.form['size'] == '':
+         
+        
+        return render_template('no_info.html')
+    
+    if request.form['sickness'] == '' and  request.form['size'] != '':
+         
+        
+        return render_template('no_info.html')
+    
+  
+         
+        
+        return render_template('no_info.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
